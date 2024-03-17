839720a45e0e2d9c7f4f741d6ea46850c7f45b00
MPT-B <maciej.biegan@student.pk.edu.pl>
2024-03-17 18:50:37 +0100

    chore:fixed project structure

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <header className="App-header">
+            <p>
+            Edit <code>src/main/frontend/src/App.js</code> and save to reload.
+            </p>
+            <a
+            className="App-link"
+            href="https://spring.io/guides/tutorials/react-and-spring-data-rest/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React with Spring Boot
+            </a>
+        </header>
+    </div>
+  );
+}
+
+export default App;
